6eb9ee9ad103a742882ad1be212a740590cf32ba
Armstrong <armstrongsouljah@gmail.com>
2021-07-20 22:17:13 +0300

    Add user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,10 @@
 const User = require('../models/user');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const {SECRET_KEY} = process.env
 
 const userSchema = Joi.object({
     username: Joi.string().min(6).required(),
@@ -28,6 +32,43 @@ const registerUser = async (data) => {
     }
 }
 
+const userLogin = async (data) => {
+   const {error} = await userSchema.validate(data);
+   if(error) return {error}
+   const {username, password} = data;
+   const user = await User.findOne({username})
+   
+   if(!user) {
+       return {error: {
+           message: "User not exist"
+       }}
+   }
+
+   const passwordMatched = await bcrypt.compare(password, user.password);
+
+   if(passwordMatched) {
+       console.log('logged in ', passwordMatched)
+       const token = jwt.sign({
+           username
+       }, SECRET_KEY, {
+           expiresIn:"1 day"
+       })
+       return{
+           data: {
+            message: "Successfully logged in",
+            token
+        }
+       } 
+   } else {
+       return {
+           error: {
+               message: "Invalid username/password."
+           }
+       }
+   }
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    userLogin
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,6 +18,23 @@ router.post('/register', async (req, res) => {
             message: "Account successfully created",
         })
     }
+});
+
+router.post('/login', async (req, res) => {
+    const {username, password } = req.body
+    const {error, data} = await authController.userLogin({
+        username,
+        password
+    })
+    if(error) {
+        return res.status(400).json({
+            error
+        })
+    } else {
+        return res.status(200).json({
+            data
+        })
+    }
 })
 
 module.exports = router;
