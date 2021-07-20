f8c1537c4f5c2084fcba14b3adda9d48ecc6de16
Armstrong <armstrongsouljah@gmail.com>
2021-07-20 22:20:03 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mogooose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+
 const todoRoutes = require('./routes/todoRoutes')
 const authRoutes = require('./routes/authRoutes')
-require('dotenv').config()
+
 
 const { DATABASE_URL } = process.env
 const { PORT } = process.env || 4000
@@ -12,6 +15,7 @@ const app = express()
 // add middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
 
 app.use('/todos', todoRoutes, (next) => {
     next()
